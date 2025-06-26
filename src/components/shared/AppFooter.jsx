f336c6a11016f832ec0063e6526028845a0ba936
Shivansh Joshi <shivanshjoshi2922@gmail.com>
2025-06-26 10:32:29 +0530

    portfolio

--- src/components/shared/AppFooter.jsx
+++ src/components/shared/AppFooter.jsx
@@ -8,20 +8,20 @@ import {
 import AppFooterCopyright from './AppFooterCopyright';
 
 const socialLinks = [
-	{
-		id: 1,
-		icon: <FiGlobe />,
-		url: 'https://www.stoman.me/',
-	},
-	{
-		id: 2,
-		icon: <FiGithub />,
-		url: 'https://github.com/realstoman',
-	},
+	// {
+	// 	id: 1,
+	// 	icon: <FiGlobe />,
+	// 	url: 'https://www.stoman.me/',
+	// },
+	// {
+	// 	id: 2,
+	// 	icon: <FiGithub />,
+	// 	url: 'https://github.com/realstoman',
+	// },
 	{
 		id: 3,
 		icon: <FiTwitter />,
-		url: 'https://twitter.com/realstoman',
+		url: 'https://x.com/Maharaj_29?t=cZyHx_0FfBcz8bbE2iga9A&s=09',
 	},
 	{
 		id: 4,
@@ -31,7 +31,7 @@ const socialLinks = [
 	{
 		id: 5,
 		icon: <FiYoutube />,
-		url: 'https://www.youtube.com/c/realstoman',
+		url: 'https://youtube.com/@kamal_deep29?si=gY9FtZfEpmd1KbCh',
 	},
 ];
 
